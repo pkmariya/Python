e010430e84a195e672b51118786f1324bb73fccf
Mariya <pkmariya@yahoo.com>
2022-10-06 15:54:23 +0530

    code changes

--- PyShuffle.py
+++ PyShuffle.py
@@ -7,4 +7,17 @@ print("Before Sorting:", cities)
 sortedCities = sorted(cities)
 print("After Sorting:", sortedCities)
 shuffle(cities)
-print(cities)
+print("Just shuflle the strings", cities)
+
+
+myList = [3, 2, 10, 3, 1, 1, 5, 7]
+
+print(myList)
+
+# .pop removes all the occurrence of given element
+myList.pop(3)
+
+# .remove removes only the first occurrence of given element
+myList.remove(1)
+
+print(myList)
